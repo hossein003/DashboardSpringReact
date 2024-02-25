beffc9dd78b76c18f5c1a54b6e39ca9b16f0ed1e
Hossein Salehi <90480508+hossein003@users.noreply.github.com>
2024-02-25 23:12:23 +0330

    add-plugin-and-automating-some-task
    Adding some plugins and automating some tasks with maven

--- src/main/java/com/workout/DashboardSpringReact/DashboardSpringReactApplication.java
+++ src/main/java/com/workout/DashboardSpringReact/DashboardSpringReactApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DashboardSpringReactApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(DashboardSpringReactApplication.class, args);
 	}
 
