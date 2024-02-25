9687768bf82ed04481a527a460eea84aed2944b0
Hossein Salehi <90480508+hossein003@users.noreply.github.com>
2024-02-25 11:24:28 +0330

    Implementation base front

---
+++ src/frontend/src/App.js
@@ -0,0 +1,136 @@
+import {getAllStudents} from "./client";
+import './App.css';
+import React, {useState, useEffect} from "react";
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import {Breadcrumb, Layout, Menu, theme, Table} from 'antd';
+
+function getItem(label, key, icon, children) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
+}
+
+const items = [
+    getItem('Option 1', '1', <PieChartOutlined/>),
+    getItem('Option 2', '2', <DesktopOutlined/>),
+    getItem('User', 'sub1', <UserOutlined/>, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined/>, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined/>),
+];
+const {Header, Content, Footer, Sider} = Layout;
+const {SubMenu} = Menu;
+const columns = [
+    {
+        title: 'Id',
+        dataIndex: 'id',
+        key: 'id',
+    },
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    },
+    {
+        title: 'Gender',
+        dataIndex: 'gender',
+        key: 'gender',
+    },
+];
+
+function App() {
+    const [students, setStudents] = useState([]);
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: {colorBgContainer, borderRadiusLG},
+    } = theme.useToken();
+    const fetchStudents = () =>
+        getAllStudents()
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setStudents(data);
+            });
+    useEffect(() => {
+        console.log("component is mounted");
+        fetchStudents();
+    }, []);
+
+    const renderStudent = () => {
+        if (students.length <= 0){
+            return "No data available";
+        }
+        return <Table dataSource={students} columns={columns} />;
+    }
+
+    return (
+        <Layout
+            style={{
+                minHeight: '100vh',
+            }}
+        >
+            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                <div className="demo-logo-vertical"/>
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items}/>
+            </Sider>
+            <Layout>
+                <Header
+                    style={{
+                        padding: 0,
+                        background: colorBgContainer,
+                    }}
+                />
+                <Content
+                    style={{
+                        margin: '0 16px',
+                    }}
+                >
+                    <Breadcrumb
+                        style={{
+                            margin: '16px 0',
+                        }}
+                    >
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div
+                        style={{
+                            padding: 24,
+                            minHeight: 360,
+                            background: colorBgContainer,
+                            borderRadius: borderRadiusLG,
+                        }}>
+                        {renderStudent()}
+                    </div>
+                </Content>
+                <Footer
+                    style={{
+                        textAlign: 'center',
+                    }}
+                >
+                    Hossein Design ©{new Date().getFullYear()}
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+}
+
+
+export default App;
